679a87e4a80f75befedc659b9ccf6855577660ae
LudmilKirov <ludmil9595@gmail.com>
2019-07-25 14:34:09 +0300

    add database

--- app/src/main/java/com/example/criminalintent/CrimeLab.java
+++ app/src/main/java/com/example/criminalintent/CrimeLab.java
@@ -10,6 +10,7 @@ import java.nio.charset.MalformedInputException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
 import com.example.criminalintent.CrimeDbSchema.CrimeTable;
 
 
@@ -29,79 +30,93 @@ public class CrimeLab {
         }
         return sCrimeLab;
     }
+
     //Add crimes to the list
-    public void addCrime(Crime c){
-        ContentValues values=getContentValues(c);
-        mDatabase.insert(CrimeTable.NAME,null,values);
+    public void addCrime(Crime c) {
+        ContentValues values = getContentValues(c);
+        mDatabase.insert(CrimeTable.NAME, null, values);
 
     }
 
     private CrimeLab(Context context) {
 
         mContext = context.getApplicationContext();
-        mDatabase=new CrimeBaseHelper(mContext).getWritableDatabase();
+        mDatabase = new CrimeBaseHelper(mContext).getWritableDatabase();
 
     }
+
     //Using a object easily can if later
     // using different list to be changed
     public List<Crime> getCrimes() {
-       List<Crime> crimes= new ArrayList<>();
-       CrimeCursorWrapper cursor=queryCrimes(null,null);
+        List<Crime> crimes = new ArrayList<>();
+        CrimeCursorWrapper cursor = queryCrimes(null, null);
 
-       try{
-           cursor.moveToFirst();
-           while(!cursor.isAfterLast()){
-               crimes.add(cursor.getCrime());
-               cursor.moveToNext();
-           }
-       }finally {
-           cursor.close();
-       }
-       return crimes;
+        try {
+            //Start with first one and isAfterLast tells
+            // that the pointer is off the end of the datasheet
+            cursor.moveToFirst();
+            while (!cursor.isAfterLast()) {
+                crimes.add(cursor.getCrime());
+                cursor.moveToNext();
+            }
+        } finally {
+            //Close the cursor
+            cursor.close();
+        }
+        return crimes;
     }
 
     //Get particular crime
     public Crime getCrime(UUID id) {
 
-    CrimeCursorWrapper cursor=queryCrimes(CrimeTable.Cols.UUID+"= ?",new String[]{id.toString()});
-    try {
-        if(cursor.getCount()==0){
-            return null;
+        CrimeCursorWrapper cursor = queryCrimes(CrimeTable.Cols.UUID + "= ?", new String[]{id.toString()});
+        try {
+            if (cursor.getCount() == 0) {
+                return null;
+            }
+            cursor.moveToFirst();
+            return cursor.getCrime();
+        } finally {
+            cursor.close();
         }
-        cursor.moveToFirst();
-        return cursor.getCrime();
-    }
-    finally {
-        cursor.close();
-    }
     }
 
-    public void updateCrime(Crime crime){
+    public void updateCrime(Crime crime) {
         String uuidString = crime.getID().toString();
         ContentValues values = getContentValues(crime);
 
-        mDatabase.update(CrimeTable.NAME,values,
-                CrimeTable.Cols.UUID+" = ?",
+        //If you put the string directly not using
+        // ? and String [],String itself might itself
+        // contain SQL code.It is called SQL injection attack.
+        //If you use ? it will treat the string as a
+        // value not code.So always use ?
+        mDatabase.update(CrimeTable.NAME, values,
+                CrimeTable.Cols.UUID + " = ?",
                 new String[]{uuidString});
     }
-    //Delete a crime
-//   // public void deleteCrime(Crime crime) {
-//        mCrimes.remove(crime);
-//    }
+
+    // Delete a crime
+    public void deleteCrime(Crime crime) {
+        String uuidString = crime.getID().toString();
+        //Using ? to prevent sql injection
+        mDatabase.delete(CrimeTable.NAME,
+                CrimeTable.Cols.UUID + " = ?",
+                new String[]{uuidString});
+    }
 
     //To put in the database
-    private  static ContentValues getContentValues(Crime crime){
+    private static ContentValues getContentValues(Crime crime) {
         ContentValues values = new ContentValues();
-        values.put(CrimeTable.Cols.UUID,crime.getID().toString());
-        values.put(CrimeTable.Cols.TITLE,crime.getTitle());
-        values.put(CrimeTable.Cols.DAVE,crime.getDate().getTime());
-        values.put(CrimeTable.Cols.SOLVED,crime.isSolved()?1:0);
+        values.put(CrimeTable.Cols.UUID, crime.getID().toString());
+        values.put(CrimeTable.Cols.TITLE, crime.getTitle());
+        values.put(CrimeTable.Cols.DAVE, crime.getDate().getTime());
+        values.put(CrimeTable.Cols.SOLVED, crime.isSolved() ? 1 : 0);
 
         return values;
     }
 
-    private CrimeCursorWrapper queryCrimes(String whereClause,String [] whereArgs){
-        Cursor cursor=mDatabase.query(
+    private CrimeCursorWrapper queryCrimes(String whereClause, String[] whereArgs) {
+        Cursor cursor = mDatabase.query(
                 CrimeTable.NAME,
                 null,//Columns - null selects all columns
                 whereClause,
@@ -113,4 +128,4 @@ public class CrimeLab {
         return new CrimeCursorWrapper(cursor);
     }
 
-    }
+}
